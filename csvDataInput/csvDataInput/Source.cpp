5025b4eef8fabaee5871fce72453dc6d9a990de0
nakai0222 <nanacololabi@gmail.com>
2016-02-09 17:53:17 +0900

    transform string ->double on array data

--- csvDataInput/csvDataInput/Source.cpp
+++ csvDataInput/csvDataInput/Source.cpp
@@ -3,9 +3,83 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cctype>
+#include <algorithm>
 using namespace std;
 
+void csvDataInput(vector<vector<double>> &csv_data);
+
 int main() {
+
+	vector<vector<double>> csv_data;
+	csvDataInput(csv_data);
+	cout << csv_data.size() << endl;
+
+	//ifstream file("TrackingData_2016-02-05.csv");
+	//vector<vector<string>> values;
+	//string str;
+	//int p;
+
+	//if (file.fail()) {
+	//	cerr << "failed." << endl;
+	//	exit(0);
+	//}
+
+	//while (getline(file, str)) {
+	//	//コメント箇所は除く
+	//	if ((p = str.find("comment")) != str.npos) continue;
+	//	vector<string> inner;
+
+	//	//コンマがあるかを探し、そこまでをvaluesに格納
+	//	while ((p = str.find(",")) != str.npos) {
+	//		inner.push_back(str.substr(0, p));
+	//		//strの中身を","の1文字後まで飛ばす
+	//		str = str.substr(p + 1);
+	//	}
+	//	inner.push_back(str);
+	//	values.push_back(inner);
+	//}
+	///*
+	//for (unsigned int i = 0; i < values.size(); ++i) {
+	//	for (unsigned int j = 0; j < values[i].size(); ++j) {
+	//		cout << values[i][j] << ",";
+	//	}
+	//	cout << endl;
+	//	//getchar();
+	//}
+	//*///
+
+	////string->double vectorとして持つ。
+	//vector<vector<double>> csv_data;
+	//vector<double> inner_d;
+	//for (unsigned int i = 0; i < values.size(); ++i) {
+	//	inner_d.clear();
+	//	for (unsigned int j = 0; j < values[i].size(); ++j) {
+	//		if (std::all_of(values[i][j].cbegin(), values[i][j].cbegin()+1, isdigit))
+	//		{
+	//			// string の構成文字列が全て数字の場合に true になります。
+	//			inner_d.push_back(stof(values[i][j]));
+	//		}
+	//	}
+	//	if(inner_d.size() > 0)
+	//	csv_data.push_back(inner_d);
+	//}
+	//for (int i = 0; i < csv_data.size(); ++i) {
+	//	cout << csv_data[i].size() << endl;
+	//	/*
+	//	for (int j = 0; j < csv_data[i].size(); ++j) {
+	//		std::cout << csv_data[i][j] << std::endl;
+	//	}
+	//	cout << endl;
+	//	*/
+	//}
+	
+	return 0;
+}
+
+
+void csvDataInput(vector<vector<double>> &csv_data) {
+
 	ifstream file("TrackingData_2016-02-05.csv");
 	vector<vector<string>> values;
 	string str;
@@ -30,25 +104,19 @@ int main() {
 		inner.push_back(str);
 		values.push_back(inner);
 	}
-	/*
-	for (unsigned int i = 0; i < values.size(); ++i) {
-		for (unsigned int j = 0; j < values[i].size(); ++j) {
-			cout << values[i][j] << ",";
-		}
-		cout << endl;
-		//getchar();
-	}
-	*///
-
 	//string->double vectorとして持つ。
-	vector<vector<double>> csv_data;
-	
+	vector<double> inner_d;
 	for (unsigned int i = 0; i < values.size(); ++i) {
+		inner_d.clear();
 		for (unsigned int j = 0; j < values[i].size(); ++j) {
-			if ((p = str.find("comment")) != str.npos) continue;
-			csv_data[i].push_back(stof(values[i][j]));
+			if (std::all_of(values[i][j].cbegin(), values[i][j].cbegin() + 1, isdigit))
+			{
+				// string の構成文字列が全て数字の場合に true になります。
+				inner_d.push_back(stof(values[i][j]));
+			}
 		}
+		if (inner_d.size() > 0)
+			csv_data.push_back(inner_d);
 	}
 
-	return 0;
 }
